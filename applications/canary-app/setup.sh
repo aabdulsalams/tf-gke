95884e698336065f3104bc4bf3d960c0330d58e8
aabdulsalams <abdulsalamsabil@gmail.com>
2024-07-10 23:43:37 +0700

    add simple application

---
+++ applications/canary-app/setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/paulhopkins11/helm-canary.git
+cd helm-canary
+bash 1_release_to_blue.sh
+
+# https://stackoverflow.com/questions/59261340/helm-init-has-been-removed-in-version-3 
---
+++ applications/simple-app/setup.sh
@@ -0,0 +1,4 @@
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f rbac.yaml
+kubectl apply -f ingress.yaml
